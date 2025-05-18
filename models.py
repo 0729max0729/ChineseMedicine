c8a7ecfaed97d8fa0704a57aecefd2bb9474f190
0729max0729 <0729max0729@gmail.com>
2025-05-18 17:45:24 +0800

    shop

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(512), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
